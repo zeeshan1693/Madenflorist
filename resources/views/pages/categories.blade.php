828cff4b31d73f2d9a9d438e72f449098f79290a
mohd zeeshan <mohd.zeeshan@sayeghwater.com>
2022-09-25 22:51:53 +0300

    sidebar - menues text change

--- resources/views/pages/categories.blade.php
+++ resources/views/pages/categories.blade.php
@@ -5,16 +5,13 @@
     <div id="page-wrapper">
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Payment Method</h1>
+                <h1 class="page-header">Categories</h1>
             </div>
         </div>
         
         <div class="row">
             <div class="col-lg-12">
                 <div class="panel panel-default">
-                    <div class="text-right">
-                        <a href="#" class="btn btn-maden">.xls</a>
-                    </div><br>
                     <div class="panel-body backgSet">
                         <p>Categories Page Content</p>
                     </div>
