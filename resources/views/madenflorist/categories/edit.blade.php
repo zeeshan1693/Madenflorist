ad7a7df0c7037f96025a2f98bfcd2f768b637595
mohd zeeshan <mohd.zeeshan@sayeghwater.com>
2022-09-28 20:34:46 +0300

    categories -> Remove dynamic css and js from create and edit page

--- resources/views/madenflorist/categories/edit.blade.php
+++ resources/views/madenflorist/categories/edit.blade.php
@@ -50,15 +50,7 @@
 @endsection
 
 @section('page-styles')
-<link href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css" rel="stylesheet">
 @stop
 
 @section('page-script')
-<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
-<script>
-    $(document).ready(function () {
-        $('#example').DataTable();
-    });
-</script>
 @stop
